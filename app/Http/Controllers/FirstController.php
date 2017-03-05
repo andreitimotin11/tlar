512f489ec9904ecb9e799feb6cf54e15261ddc57
Andrei Timotin <andreitimotin11@gmail.com>
2017-03-05 15:14:42 +0200

    First View

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrei
+ * Date: 05.03.2017
+ * Time: 15:10
+ */
+
+namespace app\Http\Controllers;
+
+//use app\Http\Controllers\Controller;
+
+
+class FirstController extends Controller
+{
+
+}
